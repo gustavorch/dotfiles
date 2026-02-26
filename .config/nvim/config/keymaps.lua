6b02f640aab439b2e85cfaba9c255b31ef65464e
Gustavo Rocha <grself@tutanota.com>
2026-02-25 21:28:53 -0300

    nada nunca acontece

--- .config/nvim/config/keymaps.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_keymap("n", "<F2>", ":w<CR>:!python3 %<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<F2>", function()
+  vim.cmd("w")
+  vim.cmd("!python3 %")
+end, { desc = "Run python file" })
